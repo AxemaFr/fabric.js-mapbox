4ef8f220b0d98bd5ecc60dd3076a49a164a54c6d
Artem Miazitov <artem@perare.io>
2024-05-26 02:35:08 +0300

    Published branch "master"

--- e2e/tests/template-with-node/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Runs in the **BROWSER**
- */
-
-import * as fabric from 'fabric';
-import { beforeAll } from '../test';
-import { render } from './common';
-
-beforeAll((canvas) => render(canvas, fabric), { enableRetinaScaling: false });
--- e2e/utils/promiseSequence.ts
+++
@@ -1,2 +0,0 @@
-export const promiseSequence = <T>(tasks: (() => Promise<T>)[]) =>
-  tasks.reduce((prev, curr) => prev.finally(curr), Promise.resolve());
